f2bbc3b3654dafb748870a1826611ce2b72f92d8
Pathomphop-pp <pathomphop.pp@gmail.com>
2025-09-14 04:47:42 +0700

    Update love_preaw.py

--- love_preaw.py
+++ love_preaw.py
@@ -211,7 +211,8 @@ if wedding_date > today.date():
         f"อีก <b>{days_to_wedding} วัน</b> จะถึงวันแต่งงานของเรา 💍✨</p>",
         unsafe_allow_html=True
     )
-    st.progress(1 - (days_to_wedding / 365))
+    progress_value = max(0.0, min(1.0, 1 - (days_to_wedding / 365)))
+    st.progress(progress_value)
 elif wedding_date == today.date():
     st.markdown(
         "<p style='font-size:20px; text-align:center; color:red;'>"
